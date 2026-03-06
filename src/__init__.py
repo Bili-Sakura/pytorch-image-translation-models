9c40f22bb2ca86a835a92683c89d8faa93affac0
Sakura <108928239+Bili-Sakura@users.noreply.github.com>
2026-03-06 08:22:39 +0800

    Bump version from 0.1.0 to 0.1.1

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ A library for multi-modal image translation with diffusion bridges,
 GANs, and transformer backbones.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from src.data import PairedImageDataset, UnpairedImageDataset, default_transforms, get_transforms
 from src.inference import ImageTranslator
